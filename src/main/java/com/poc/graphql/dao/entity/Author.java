a08d83dc99ece41f974f20e89c31e09552aff8ef
Chaitanya Pathak (OSV) <chaitanyapathak.osv@fedex.com>
2019-09-21 11:14:40 +0200

    Addin complex object structures

--- src/main/java/com/poc/graphql/dao/entity/Author.java
+++ src/main/java/com/poc/graphql/dao/entity/Author.java
@@ -3,6 +3,7 @@ package com.poc.graphql.dao.entity;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
@@ -19,10 +20,11 @@ public class Author implements Serializable {
      */
     private static final long serialVersionUID = 1L;
 
+    @Id
     private String id;
     private String authorName;
     private int age;
-    private String writesIn;
+    private List<Language> writesIn;
     private List<Post> posts;
 
 }
