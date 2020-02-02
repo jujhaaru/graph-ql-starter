14474340758388eb7b9e7d3c4af68076ed71def6
Chaitanya Pathak (OSV) <chaitanyapathak.osv@fedex.com>
2020-02-03 02:26:29 +0530

    added eureka-client tested highly available eureka server

--- src/main/java/com/poc/graphql/GraphQlStarterApplication.java
+++ src/main/java/com/poc/graphql/GraphQlStarterApplication.java
@@ -4,9 +4,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GraphQlStarterApplication implements CommandLineRunner {
 
 	private static final Logger LOG= LoggerFactory.getLogger("GraphQlStarterApplication.class");
